bad581cb1dcebce2e4e6dfbf7dfb186925c2c4aa
Jure Bogunovic <bogunovij@gmail.com>
2022-01-26 03:19:31 +0100

    created api for nft metadata

---
+++ api/src/runServer.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import http from "http";
+
+export const runServer = (port: number) => {
+  const app = express();
+
+  app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    //Trim and redirect multiple slashes in URL
+    if(req.url.match(/[/]{2,}/g)) {
+      req.url = req.url.replace(/[/]+/g, '/');
+      res.redirect(req.url);
+      return;
+    } 
+
+    if (req.method === 'OPTIONS') {
+      res.send(200);
+    } else {
+      console.log("Request: " + req.method + " " + req.url);
+      next();
+    }
+  });
+
+  app.get('/api/nft/:id', async (req, res) => {
+    const nftId = req.params.id;
+
+    res.json({
+      description: "A test NFT.", 
+      external_url: "", 
+      image: "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png", 
+      name: "Tester",
+      attributes: [], 
+    });
+  });
+
+  app.get("/", async (req, res) => {
+    res.send("Hello world!");
+  });
+
+  const server = http.createServer({}, app);
+  
+  server.listen(port, function(){
+    console.log(`HTTP server started at http://localhost:${port}` );
+  });
+};

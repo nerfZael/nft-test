f2ebe41dc9f83c8e68166e0e1317ad4007171ff6
Jure Bogunovic <bogunovij@gmail.com>
2022-02-26 20:32:11 +0100

    improved comments for RelayRecipientAndContextMixin

--- nft-sale/hardhat/contracts/common/meta-transactions/RelayRecipientAndContextMixin.sol
+++ nft-sale/hardhat/contracts/common/meta-transactions/RelayRecipientAndContextMixin.sol
@@ -5,6 +5,13 @@ pragma solidity ^0.8.11;
 import "./RelayRecipient.sol";
 
 abstract contract RelayRecipientAndContextMixin is RelayRecipient {
+    /**
+     * Return the sender of this call.
+     * if the call came through a trusted forwarder (EIP-2771), return the original sender.
+     * if the call came from the contract itself (EIP-712 meta transactions), return the original sender.
+     * otherwise, return `msg.sender`.
+     * should be used in the contract anywhere instead of msg.sender
+     */
     function msgSender()
         internal
         view
@@ -18,6 +25,7 @@ abstract contract RelayRecipientAndContextMixin is RelayRecipient {
                 sender := shr(96,calldataload(sub(calldatasize(),20)))
             }
         } else if (msg.sender == address(this)) {
+            // If the sender is the contract itself, then it's using the EIP-712 meta transactions.
             bytes memory array = msg.data;
             uint256 index = msg.data.length;
             assembly {
@@ -33,11 +41,4 @@ abstract contract RelayRecipientAndContextMixin is RelayRecipient {
 
         return sender;
     }
-
-    /**
-     * return the sender of this call.
-     * if the call came through a trusted forwarder, return the original sender.
-     * otherwise, return `msg.sender`.
-     * should be used in the contract anywhere instead of msg.sender
-     */
 }
